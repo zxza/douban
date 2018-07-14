8a262dc41aaa51e7650026befdc106b51a1d0ccb
zxza <arzh@royole.com>
2018-07-14 16:58:31 +0800

    修改loading效果

---
+++ src/store/api.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const HOST = process.env.NODE_ENV === 'production' ? 'https://node-douban-api.herokuapp.com' : 'http://localhost:8080';
+
+export const API_TYPE = {
+  movie : {
+    in_theaters: 'in_theaters',
+    coming_soon: 'coming_soon'
+  }
+};
+
+export function fetch(url) {
+
+  return new Promise((resolve, reject) => {
+    axios.get(HOST + url)
+      .then(response => {
+        resolve(response.data);
+      })
+  })
+}
+
+export function fetchItemByType() {
+  return fetch(`${type}`);
+}
+
+export function fetchMoviesByType(type, start = 0, count = 20) {
+  return fetchItemByType(`/movie/${type}?start=${start}&count=${count}`)
+}
--- src/views/movies.vue
+++ src/views/movies.vue
@@ -1,13 +1,43 @@
 <template lang="html">
-  <div class="movies">
-    <div class="">
-      123123123
-    </div>
+  <div>
+    <section class="grid has-search-bar">
+      <div v-if="inTheater.title">
+        <h2>{{inTheater.title}}
+          <router-link :to="{ name: '', params: {} }"></router-link>
+        </h2>
+        <div class="card">
+          <router-link :to="{ name: '', params: {} }">
+
+          </router-link>
+        </div>
+      </div>
+      <Spinner :show='loading'></Spinner>
+    </section>
   </div>
 </template>
 
 <script>
+import Spinner from '@/components/Spinner'
+import {mapState} from 'vuex'
+import {API_TYPE, fetchMoviesByType} from '@/store/api'
 export default {
+  components: {
+    Spinner
+  },
+  data() {
+    return {
+      loading: true
+    }
+  },
+  mounted() {
+
+  },
+  updated() {
+
+  },
+  destroyed() {
+
+  }
 }
 </script>
 
